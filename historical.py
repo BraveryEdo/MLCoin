bd9f7e2554c7ae19a07bff5f71cc0d74b3262d1a
BraveryEdo <Braveryedo@gmail.com>
2022-02-20 15:19:34 -0500

    breaking down the historical data prob into manageable parts
    lunch break

--- historical.py
+++ historical.py
@@ -12,12 +12,19 @@ CONTINUE_TRAINING = True
 REST_API = 'https://api.pro.coinbase.com'
 PRODUCTS = REST_API+'/products'
 csv_path = 'csv_data'
+start_year = 2015
+# I am only interested in a few currencies that I want to trade, so let's add them here:
+#MY_CURRENCIES = ['BTC-USD','ETH-USD','LTC-USD','DOGE-USD','SHIB-USD','ALGO-USD','MANA-USD','MATIC-USD']
+MY_CURRENCIES = ['BTC-USD']
+TIME_SCALE = '1hr'
+
+time_lens = {'1min':60, '5min':300, '15min':900, '1hr':3600, '6hr':21600, '1day':86400}
 
 
 
-def connect(url, *args, **kwargs):
+def connect(url, *args):
     try:
-        if kwargs.get('param', None) is not None:
+        if args is not None:
             response = requests.get(url,args)
         else:
             response = requests.get(url)
@@ -29,14 +36,94 @@ def connect(url, *args, **kwargs):
     except Exception as err:
         print(f'Other error occurred: {err}')
 
+def getData(pair, start, end, resolution):
+    #getting historical data from candles not individual trades, that might be a bit much
+    pair_path = PRODUCTS + '/' + pair + '/candles'
+    res = resolution
+    #accept either char description or integer, but default to 1hr
+    if resolution is not integer:
+        if resoulution in time_lens:
+            res = time_lens[resolution]
+        else:
+            res = time_lens['1hr'];
+    if resolution not in time_lens.values():
+        res = time_lens['1hr']
+
+    #loop through provided time range given time resolution
+
+
+    params = {'start':start, 'end':end, 'granularity':res}
+    response = connect(pair_path, params)
+    print(response.status_code)
+    return response
+
+def populateYearPath(currency, c_path, year):
+    fileList = os.listdir(c_path + f'\\{i}')
+    if fileList == []:
+        #super empty
+        response = getData(currency, start, end, TIME_SCALE)
+    else:
+        #see if partially filled, delete lastest (most likely to have an error) and restart from there
+
+        print(f'{currency}, {year}, contains: {fileList}')
+    return True
+
+def makeYearPaths(currency, c_path):
+    #make folders for each year starting with start year
+    end_year = datetime.now().year
+    for i in range (start_year, end_year+1):
+        os.mkdir(c_path + f'\\{i}')
+        print(f'made {i} path for {currency}')
+        populateYearPath(currency, c_path, i)
+        #get full data for year (or ytd for current year) and save csv(s)
+    return True
+
+def populateCurrencyPath(currency, c_path):
+    if os.listdir(c_path) == []:
+        #super empty
+        makeYearPaths(currency, c_path)
+    else:
+        #check how full the subfolders are
+    return True
+
+def makeCurrencyPath(currency, c_path):
+    os.mkdir(c_path)
+    print(f'added {currency} subfolder')
+    populateCurrencyPath(currency, c_path)
+    return True
+
+
+
+def populateRootPath():
+    for currency in MY_CURRENCIES:
+        curr_path = csv_path + '\\' + currency
+        if os.path.isdir(curr_path) == False:
+            #super empty, make the directory and fill it
+            makeCurrencyPath(currency, curr_path)
+        else:
+            #see if we have any data 
+            print(f'files in {curr_path}: ')
+            print(os.listdir(curr_path))
+            if os.listdir(curr_path) == []:
+                #super empty, make the yearly folders and fill em
+                print(f'get historical data for {currency}')
+            elif CONTINUE_TRAINING:
+                print('see how up to date our data is, fill in new data if wanted')
+            else:
+                print(f'some {currency} historical data is present, no more required at this time, edit start_year & CONTINUE_TRAINING vars if desired')
+    return True
+
+def makeRootPath():
+    os.mkdir(csv_path)
+    print('added csv_data main folder')
+    populateRootPath
+    return True
+
 def getHistorical():
     response = connect(PRODUCTS)
     response_content = response.content
     response_text = response.text
     #response_headers = response.headers
-    # I am only interested in a few currencies that I want to trade, so let's add them here:
-    MY_CURRENCIES = ['BTC-USD','ETH-USD','LTC-USD','DOGE-USD','SHIB-USD','ALGO-USD','MANA-USD','MATIC-USD']
-
     df_currencies = pandas.read_json (response_text)
     #print("\nNumber of columns in the dataframe: %i" % (df_currencies.shape[1]))
     #print("Number of rows in the dataframe: %i\n" % (df_currencies.shape[0]))
@@ -56,23 +143,8 @@ def getHistorical():
 
     #check for folder structure
     if os.path.isdir(csv_path) == False:
-        os.mkdir(csv_path)
-        print('setting up csv_data main folder')
-    for currency in MY_CURRENCIES:
-        curr_path = csv_path + '\\' + currency
-        if os.path.isdir(curr_path) == False:
-            os.mkdir(curr_path)
-            print(f'setting up {currency} subfolder')
-
-        #see if we have any data 
-        print(f'files in {curr_path}: ')
-        print(os.listdir(curr_path))
-        if os.listdir(curr_path) == []:
-            print(f'get historical data for {currency}')
-        elif CONTINUE_TRAINING:
-            print('see how up to date our data is, fill in new data if wanted')
-        else:
-            print(f'{currency} historical data is present, no more required at this time')
+        makeRootPath
+    
 
 def other():
     start_date = (datetime.today() - timedelta(days=90)).isoformat()
